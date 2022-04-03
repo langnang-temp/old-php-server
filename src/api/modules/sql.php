a7452c56cac6455132ae9e17977d2a2e43ce9f85
langnang <langnang.chen@outlook.com>
2022-04-03 17:47:04 +0800

    Update sql.php

--- src/api/modules/sql.php
+++ src/api/modules/sql.php
@@ -1,10 +1,15 @@
 <?
 
-
+/**
+ * @OA\Tag(
+ *     name="sql",
+ * )
+ */
 $router->addGroup('/sql', function (FastRoute\RouteCollector $router) {
   /**
    * @OA\Get(
    *     path="/sql/connection",
+   *     tags={"sql"},
    *     @OA\Response(response="200", description="An example resource")
    * )
    */
